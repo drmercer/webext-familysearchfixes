cc7ba3be7a5e945beb4260ff74d389c83092dbb3
Dan Mercer <danielmercer1@gmail.com>
2018-08-24 23:16:32 -0600

    Added code to track viewport settings per input field

--- src/content_scripts/indexing.js
+++ src/content_scripts/indexing.js
@@ -3,5 +3,4 @@
 	var script = document.createElement("SCRIPT");
 	script.src = browser.runtime.getURL("page_scripts/indexing.js");
 	document.body.appendChild(script);
-	console.log("Content script run");
 }());
--- src/page_scripts/indexing.js
+++ src/page_scripts/indexing.js
@@ -1,13 +1,15 @@
 (function() {
 	"use strict";
 
-	var viewer, viewport;
+	var viewer,
+		viewport,
+		fieldSettings = {};
 
 	waitForEl("idx-ribbon-viewer")
 		.then(start);
 
 	function start(ribbonViewerEl) {
-		console.log("Start");
+		console.log("ribbon viewer found");
 		// Getting viewer and viewport
 		viewer = angular
 			.element(ribbonViewerEl)
@@ -18,26 +20,40 @@
 			.getViewer();
 		viewport = viewer.viewport;
 
-		setTimeout(doDemoStuff, 3000);
+		handleViewportChange();
 
-		console.log("End");
+		viewer.addHandler('zoom', handleViewportChange);
+		viewer.addHandler('pan', handleViewportChange);
 	}
 
-	function doDemoStuff() {
-		// Zoom
-		viewport.getZoom();
-		viewport.zoomBy(3.0);
+	function handleViewportChange() {
+		var key = getCurrentFieldKey();
+		if (!key) return;
+		var pos = viewport.getCenter();
+		var zoom = viewport.getZoom();
 
-		// Pan
-		var ctr = viewport.getCenter();
-		ctr.x += 100
-		viewport.panTo(ctr);
+		fieldSettings[key] = {
+			pos: pos,
+			zoom: zoom,
+		};
+
+		console.log(fieldSettings);
+	}
+
+	function getCurrentFieldKey() {
+		var field = document.querySelector('.entry-field.focus');
+		if (!field) return null;
+		var idEl = field.querySelector('*[id^="entryFieldformEntry"]');
+		if (!idEl) return null;
+		var id = idEl.id;
+		return id;
 	}
 
 	//======================================================================
 	//			Utilities
 
 	function waitForEl(selector, interval) {
+		console.log("Waiting for " + selector);
 		interval = interval || 1000;
 		var resolve;
 		var prom = new Promise(res => resolve = res);
